446ccf264effe66fa21bdcb5f9d0a7a07e0c2e5d
alejandropj <alejandroparrajimenez@gmail.com>
2024-03-11 14:04:08 +0100

    Add project files.

---
+++ Controllers/PaginacionController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using MvcCorePaginacionRegistros.Models;
+using MvcCorePaginacionRegistros.Repositories;
+
+namespace MvcCorePaginacionRegistros.Controllers
+{
+    public class PaginacionController : Controller
+    {
+        private RepositoryHospital repo;
+        public PaginacionController(RepositoryHospital repo)
+        {
+            this.repo = repo;
+        }
+        #region Vista
+/*        CREATE VIEW V_DEPARTAMENTOS_INDIVIDUAL
+AS
+
+    SELECT CAST(
+    ROW_NUMBER() OVER (ORDER BY DEPT_NO) AS INT) AS POSICION,
+    ISNULL(DEPT_NO, 0) AS DEPT_NO, DNOMBRE, LOC FROM DEPT
+GO
+
+SELECT* FROM V_DEPARTAMENTOS_INDIVIDUAL WHERE POSICION=1*/
+        #endregion
+        public async Task<IActionResult> PaginarRegistroVistaDept(int? posicion)
+        {
+            if (posicion == null)
+            {
+                posicion = 1;
+
+            }
+            int numeroRegistros = await this.repo.GetNumeroRegistrosVistaDepts();
+            int siguiente = posicion.Value + 1;
+
+            if(siguiente > numeroRegistros)
+            {
+                siguiente = numeroRegistros;
+            }
+            int anterior = posicion.Value - 1;
+            if(anterior < 1)
+            {
+                anterior = 1;
+            }
+            VistaDepartamento vista = await this.repo.GetVistaDeptAsync(posicion.Value);
+            ViewData["ULTIMO"] = numeroRegistros;
+            ViewData["SIGUIENTE"] = siguiente;
+            ViewData["ANTERIOR"] = anterior;
+
+            return View(vista);
+        }        
+        public async Task<IActionResult> PaginarGrupoVistaDept(int? posicion)
+        {
+            if (posicion == null)
+            {
+                posicion = 1;
+
+            }
+            int numeroRegistros = await this.repo.GetNumeroRegistrosVistaDepts();
+            ViewData["REGISTROS"] = numeroRegistros;
+            List<VistaDepartamento> departamentos = await this.repo.GetGrupoVistaDeptAsync(posicion.Value);
+            return View(departamentos);
+        }              
+        public async Task<IActionResult> PaginarGrupoDept(int? posicion)
+        {
+            if (posicion == null)
+            {
+                posicion = 1;
+
+            }
+            int numeroRegistros = await this.repo.GetNumeroRegistrosVistaDepts();
+            ViewData["REGISTROS"] = numeroRegistros;
+            List<Departamento> departamentos = await this.repo.GetGrupoDeptAsync(posicion.Value);
+            return View(departamentos);
+        }        
+/*        public async Task<IActionResult> PaginarGrupoVistaDept(int? posicion)
+        {
+            if (posicion == null)
+            {
+                posicion = 1;
+
+            }
+            int numeroPagina = 1;
+            int numeroRegistros = await this.repo.GetNumeroRegistrosVistaDepts();
+            int siguiente = posicion.Value + 1;
+            string html = "<div>";
+            for(int i =1; i <=numeroRegistros; i += 2)
+            {
+                html += "<a href='PaginarGrupoVistaDept?posicion=" 
+                    + i + "'>Página " + numeroPagina + "</a>";
+                numeroPagina++;
+            }
+            html += "</div>";
+            ViewData["LINKS"] = html;
+            List<VistaDepartamento> departamentos = await this.repo.GetGrupoVistaDeptAsync(posicion.Value);
+            return View(departamentos);
+        }*/
+    }
+}
---
+++ Repositories/RepositoryHospital.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.CodeAnalysis.Elfie.Diagnostics;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using MvcCorePaginacionRegistros.Data;
+using MvcCorePaginacionRegistros.Models;
+
+namespace MvcCorePaginacionRegistros.Repositories
+{
+    public class RepositoryHospital
+    {
+        private HospitalContext context;
+
+        public RepositoryHospital(HospitalContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<int> GetNumeroRegistrosVistaDepts()
+        {
+            return await this.context.VistaDepartamentos.CountAsync();
+        }
+
+        public async Task<VistaDepartamento> GetVistaDeptAsync(int pos)
+        {
+            VistaDepartamento vista = await this.context.VistaDepartamentos.Where(z => z.Posicion == pos).FirstOrDefaultAsync();
+            return vista;
+
+        }
+        #region procedimientos almacenados
+/*        CREATE PROCEDURE SP_GRUPO_DEPARTAMENTOS
+        (@POSICION INT)
+AS
+        SELECT DEPT_NO, DNOMBRE, LOC
+    FROM V_DEPARTAMENTOS_INDIVIDUAL
+    WHERE POSICION >= @POSICION AND POSICION<(@POSICION + 2)
+GO
+
+EXEC SP_GRUPO_DEPARTAMENTOS 1*/
+        #endregion
+        public async Task<List<Departamento>> GetGrupoDeptAsync(int pos)
+        {
+            string sql = "SP_GRUPO_DEPARTAMENTOS @posicion";
+            SqlParameter pamPosicion = new SqlParameter("@posicion", pos);
+            var consulta = this.context.Departamentos.FromSqlRaw(sql, pamPosicion);
+            return await consulta.ToListAsync();
+
+        }
+        public async Task<List<VistaDepartamento>> GetGrupoVistaDeptAsync(int pos)
+        {
+            var consulta = from datos in this.context.VistaDepartamentos
+                           where datos.Posicion >= pos
+                           && datos.Posicion < (pos + 2)
+                           select datos;
+            return await consulta.ToListAsync();
+
+        }
+
+        public async Task<List<Departamento>> GetDepartamentosAsync()
+        {
+            return await this.context.Departamentos.ToListAsync();
+        }
+        public async Task<List<Empleado>> GetEmpleadosDepartamentoAsync(int idDepartamento)
+        {
+            var empleados = this.context.Empleados.Where(x=> x.IdDepartamento == idDepartamento);
+            if (empleados.Count() == 0)
+            {
+                return null;
+            }
+            else
+            {
+                return await empleados.ToListAsync();
+            }
+        }
+
+    }
+}
