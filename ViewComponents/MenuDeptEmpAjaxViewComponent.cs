9de9821a28408acce52324e16563e40130d82dd2
alejandropj <alejandroparrajimenez@gmail.com>
2024-03-13 21:30:48 +0100

    13/03

---
+++ ViewComponents/MenuDeptEmpAjaxViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCorePaginacionRegistros.Models;
+using MvcCorePaginacionRegistros.Repositories;
+
+namespace MvcCorePaginacionRegistros.ViewComponents
+{
+    public class MenuDeptEmpAjaxViewComponent:ViewComponent
+    {
+        private RepositoryHospital repo;
+        public MenuDeptEmpAjaxViewComponent(RepositoryHospital repo)
+        {
+            this.repo = repo;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            List<Departamento> depts = await this.repo.GetDepartamentosAsync();
+            return View(depts);
+        }
+    }
+}
